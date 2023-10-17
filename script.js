be6cf79660496a4ee48726a6c686e23b4c03c237
kusud-yuki <u.b_bfg@icloud.com>
2023-10-17 10:45:35 +0900

    文字の移動に合わせて背景のカラーが変化する

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-
 const textElement = document.getElementById('animatedText');
+const backgroundElement = document.getElementById('background');
 
 async function animateText() {
-    for (let i = 100; i; i--) {
-        textElement.style.transform = `translateX(${i}%)`;
-        await sleep(50); 
+    while (true) {
+        for (let i = 100; i; i--) {
+            textElement.style.transform = `translateX(${i}%)`;
+            backgroundElement.style.backgroundColor = `rgb(${255 - i * 2}, 0, 0)`;
+            await sleep(50); 
+        }
     }
 }
 
