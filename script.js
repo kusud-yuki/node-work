89f17b29874bec8461b42df73a7e1b3b461a8023
kusud-yuki <u.b_bfg@icloud.com>
2023-10-18 13:01:47 +0900

    画像や文字のサイズが変化する機能の実装

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// JavaScriptファイル（script.js）
 const textElement = document.getElementById('animatedText');
 const backgroundElement = document.getElementById('background');
+const imageElement = document.getElementById('animatedImage');
+
 const texts = [
     'Hello, World! This is a scrolling text.',
     'Welcome to our website!',
@@ -8,19 +11,24 @@ const texts = [
 ];
 
 let currentTextIndex = 0;
+let isZoomedIn = true;
 
-async function animateText() {
+async function animateTextAndImage() {
     while (true) {
         for (let i = 100; i; i--) {
             textElement.style.transform = `translateX(${i}%)`;
             backgroundElement.style.backgroundColor = `rgb(${255 - i * 2}, 0, 0)`;
-            await sleep(50); 
+            textElement.style.fontSize = `${24 + i / 4}px`;
+            imageElement.style.transform = isZoomedIn ? `scale(${i / 100})` : `scale(${1 - i / 100})`;
+            await sleep(50);
         }
 
         currentTextIndex = (currentTextIndex + 1) % texts.length;
         textElement.textContent = texts[currentTextIndex];
 
-        await sleep(1000); 
+        isZoomedIn = !isZoomedIn; // 画像のズームイン/ズームアウトを切り替え
+
+        await sleep(1000);
     }
 }
 
@@ -28,5 +36,6 @@ async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-animateText();
+animateTextAndImage();
+
 
